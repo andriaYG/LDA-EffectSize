90167b6883e81af1ec856c34dc917d63bd097a89
andriaYG <biotechyuan@outlook.com>
2017-03-29 11:35:08 +0800

    Update Kruskal.EffectSize.R

--- Kruskal.EffectSize.R
+++ Kruskal.EffectSize.R
@@ -1,82 +1,121 @@
-####Kruskal.test
-setwd("/Users/simona/SGDev/BGI/project/IBD.v2/fisher.lefse")
-read.table("M100gene.MGS.123hostFree.relative.abun.pro")->Dat;
-read.table("occ50.MGS100.list",header = F,colClasses = "character")->occ50;
-Dat[match(occ50[,1],rownames(Dat)),]->Dat
+
+read.table("M700gene.MGS.117hostFree.relative.abun.pro")->Dat;
 read.table("Sample.group.txt",header = F,sep = "\t")->label;
+Dat[,match(label[,1],colnames(Dat))]->Dat
 Result<-c();
-Res_temp<-list(mgsID = character(),statistic = numeric(),p.value = numeric(),A=numeric(),B=numeric(),C=numeric(),type = character(),A.occ=numeric(),B.occ=numeric(),C.occ=numeric())
+Res_temp<-list(mgsID = character(),statistic = numeric(),p.value = numeric(),A=numeric(),B=numeric(),C=numeric(),
+               #type = character(),
+               A.occ=numeric(),B.occ=numeric(),C.occ=numeric())
 t(Dat)->Data;
 label<-as.factor(label[,2])
 for(i in 1:dim(Data)[2]){
   colnames(Data)[i]->Res_temp$mgsID;
   kruskal.test(Data[1:dim(Data)[1],i], label)->Res;
-  Res_temp$A<-mean(Data[which(label==2),i]);
-  Res_temp$B<-mean(Data[which(label==1),i]);
-  Res_temp$C<-mean(Data[which(label==3),i]);
+  Res_temp$A<-mean(Data[which(label=="A"),i]);
+  Res_temp$B<-mean(Data[which(label=="B"),i]);
+  Res_temp$C<-mean(Data[which(label=="C"),i]);
   Res_temp$type<-"C";
   if(Res_temp$A==max(Res_temp$A,Res_temp$B,Res_temp$C)){Res_temp$type<-"A";}
   if(Res_temp$B==max(Res_temp$A,Res_temp$B,Res_temp$C)){Res_temp$type<-"B";}
-  Res_temp$A.occ<-length(which(Data[which(label==2),i]!=0))/length(Data[which(label==2),i]);
-  Res_temp$B.occ<-length(which(Data[which(label==1),i]!=0))/length(Data[which(label==1),i]);
-  Res_temp$C.occ<-length(which(Data[which(label==3),i]!=0))/length(Data[which(label==3),i]);
+  Res_temp$A.occ<-length(which(Data[which(label=="A"),i]!=0))/length(Data[which(label=="A"),i]);
+  Res_temp$B.occ<-length(which(Data[which(label=="B"),i]!=0))/length(Data[which(label=="B"),i]);
+  Res_temp$C.occ<-length(which(Data[which(label=="C"),i]!=0))/length(Data[which(label=="C"),i]);
   Res$statistic->Res_temp$statistic;
   Res$p.value->Res_temp$p.value;
-  Result<-rbind(Result,Res_temp);
+  if(Res_temp$A.occ > 0.05 && (Res_temp$B.occ > 0.05 || Res_temp$C.occ> 0.05)){
+    Result<-rbind(Result,Res_temp);
+    }
 }
-#unique(which(Result[,3]<0.01))->p01.index
 unique(which(Result[,3]<0.05))->p05.index 
-write.table(Result[p05.index,],"M100.Kruskal.p05.mgs.list",col.names = T,row.names = F,sep ="\t",quote = F)
+write.table(Result[p05.index,],"M700.Kruskal.p05.mgs.list.v2.txt",col.names = T,row.names = F,sep ="\t",quote = F)
 
-##since our data cannot fit the wilconxon test,we skip this step
-#An LDA model is finally built with the class as dependent variable and the remaining feature values, subclass, and subject values as independent variables. This model is used to estimate their effect sizes, which are obtained by averaging the differences between class means (using unmodified feature values) with the differences between class means along the first linear discriminant axis, which equally weights features' variability and discriminatory power. The LDA score for each biomarker is obtained computing the logarithm (base 10) of this value after being scaled in the [1,106] interval and, regardless of the absolute values of the LDA score, it induces the ranking of biomarker relevance. For robustness, LDA is additionally supported by bootstrapping (default 30-fold) and subsequent averaging.
-##LDA score estimating the effect side
-lda.data <- data.frame( Data[,match(Result[p01.index,1],colnames(Data))],
-                        group = label)   
+Data<-Data[,match(as.character(Result[p05.index,1]),colnames(Data))]
+read.table("Sample.group.EEN.txt",header = F,sep = "\t")->E.label
+read.table("Sample.group.CDvsCT.txt",header = F,sep = "\t")->C.label
+C.Data<-Data[match(as.character(C.label[,1]),rownames(Data)),]
+C.Data<-cbind(as.character(C.label[match(rownames(C.Data),as.character(C.label[,1])),2]),as.character(C.label[match(rownames(C.Data),as.character(C.label[,1])),3]),C.Data)
+E.Data<-Data[match(as.character(E.label[,1]),rownames(Data)),]
+E.Data<-cbind(as.character(E.label[match(rownames(E.Data),as.character(E.label[,1])),2]),as.character(E.label[match(rownames(E.Data),as.character(E.label[,1])),3]),E.Data)
+colnames(C.Data)[1:2]<-c("group","disease")
+colnames(E.Data)[1:2]<-c("group","disease")
 
-library("MASS")
-#test<-lda.data[,-c(46,48,57,70,73,74,75,87,88,90,96,194,202)]
-z <- lda(group ~ ., lda.data, prior = c(1,1,1)/3)
-##abs(z$scaling[,"LD1"])/sum(abs(z$scaling[,"LD1"]))->sca.LD1
-##=============================================
-#lda.re<-list(mgsID = character(),lda.score = numeric())
-#test<-c();
-#for(i in 1:dim(z$means)[2]){
-#	dimnames(lda.data)[[2]][i]->lda.re$mgsID;
-#	Dis1<-z$means[1,i]-z$means[2,i]
-#	Dis2<-z$means[2,i]-z$means[3,i]
-#	Dis3<-z$means[1,i]-z$means[3,i]
-#	#log(sqrt((Dis1^2+Dis2^2+Dis3^2)*abs(z$scaling[i,1])/3),10)->lda.re$lda.score
-#	sqrt((Dis1^2+Dis2^2+Dis3^2)*abs(z$scaling[i,1])/3)->lda.re$lda.score
-#	test<-rbind(test,lda.re)
-#}
-
-
-lda.re<-list(mgsID = character(),lda.score = numeric())
-lda.result<-c();
-length(which(lda.data[,"group"]=="1"))->n1;
-length(which(lda.data[,"group"]=="2"))->n2;
-length(which(lda.data[,"group"]=="3"))->n3;
-for(i in 1:dim(z$means)[2]){
-  dimnames(lda.data)[[2]][i]->lda.re$mgsID;
-  Dis1<-abs(z$means[1,i]-z$means[2,i]);
-  Dis2<-abs(z$means[2,i]-z$means[3,i]);
-  Dis3<-abs(z$means[1,i]-z$means[3,i]);
-  sum((lda.data[which(lda.data[,"group"]=="1"),i]-z$means[1,i])^2)/(n1-1)->s1;
-  if(s1==0){s1<-(1e-10)}
-  sum((lda.data[which(lda.data[,"group"]=="2"),i]-z$means[2,i])^2)/(n2-1)->s2;
-  if(s2==0){s2<-(1e-10)}
-  sum((lda.data[which(lda.data[,"group"]=="3"),i]-z$means[3,i])^2)/(n3-1)->s3;
-  if(s3==0){s3<-(1e-10)}
-  s12<-sqrt(((n1-1)*s1+(n2-1)*s2)/(n1+n2-2))
-  s23<-sqrt(((n2-1)*s2+(n3-1)*s3)/(n3+n2-2))
-  s13<-sqrt(((n1-1)*s1+(n3-1)*s3)/(n1+n3-2))
-  #log(sqrt((Dis1^2+Dis2^2+Dis3^2)*abs(z$scaling[i,1])/3),10)->lda.re$lda.score
-  log(abs(z$scaling[i,1])*(Dis1/s12+Dis2/s23+Dis3/s13),10)->lda.re$lda.score
-  lda.result<-rbind(lda.result,lda.re)
+calculator<-function(Dat,index,g1,g2){
+  res<-list(clinical=character(),pvalue=numeric(),enrichment=character(),mean1=numeric(),mean2=numeric(),occ1=numeric(),occ2=numeric())
+  Result<-c()
+  for(i in 3:ncol(Dat)){
+    #if(sum(as.numeric(Dat[which(Dat[,index]==g1),i]))!=0 && sum(as.numeric(Dat[which(Dat[,index]==g2),i]))!=0){
+    res$clinical<-colnames(Dat)[i]
+    res$mean1<-mean(as.numeric(Dat[which(Dat[,index]==g1),i]))
+    res$mean2<-mean(as.numeric(Dat[which(Dat[,index]==g2),i]))
+    if(res$mean1>res$mean2){res$enrichment<-g1}
+    if(res$mean1<res$mean2){res$enrichment<-g2}
+    if(res$mean1==res$mean2){res$enrichment<-"NA"}
+    res$occ1<-length(which(as.numeric(Dat[which(Dat[,index]==g1),i])!=0))/length(Dat[which(Dat[,index]==g1),i])
+    res$occ2<-length(which(as.numeric(Dat[which(Dat[,index]==g2),i])!=0))/length(Dat[which(Dat[,index]==g2),i])
+    res$pvalue<-wilcox.test(as.numeric(Dat[which(Dat[,index]==g1),i]),as.numeric(Dat[which(Dat[,index]==g2),i]))$p.value
+    Result<-rbind(Result,res)
+    #}
+  }
+  q.value<-p.adjust(Result[,2])
+  Result<-cbind(Result[,c(1,2)],q.value,Result[,3:7])
+  return(Result)
 }
+C.Data[which(C.Data[,"disease"]=="CT"),]->CT.Dat;
+calculator(CT.Dat,1,"A","B")->CT.result;
+C.Data[union(intersect(which(C.Data[,"group"]=="A"),which(C.Data[,"disease"]=="CT")),intersect(which(C.Data[,"group"]=="B"),which(C.Data[,"disease"]=="CD"))),]->A.CTvsB.CD.Dat;
+calculator(A.CTvsB.CD.Dat,1,"A","B")->A.CTvsB.CD.result;
+C.Data[union(intersect(which(C.Data[,"group"]=="A"),which(C.Data[,"disease"]=="CT")),intersect(which(C.Data[,"group"]=="C"),which(C.Data[,"disease"]=="CD"))),]->A.CTvsC.CD.Dat;
+calculator(A.CTvsC.CD.Dat,1,"A","C")->A.CTvsC.CD.result;
+C.Data[which(C.Data[,"group"]=="B"),]->B.Dat;
+calculator(B.Dat,2,"CT","CD")->B.result;
+C.Data[union(intersect(which(C.Data[,"group"]=="B"),which(C.Data[,"disease"]=="CT")),intersect(which(C.Data[,"group"]=="C"),which(C.Data[,"disease"]=="CD"))),]->B.CTvsC.CD.Dat;
+calculator(B.CTvsC.CD.Dat,1,"B","C")->B.CTvsC.CD.result;
+C.Data[which(C.Data[,"disease"]=="CD"),]->B.CDvsC.CD.Dat;
+B.CDvsC.CD.Dat[-which(B.CDvsC.CD.Dat[,"group"]=="A"),]->B.CDvsC.CD.Dat
+calculator(B.CDvsC.CD.Dat,1,"B","C")->B.CDvsC.CD.result;
+calculator(E.Data,2,"1pre","2post")->PreEENvsPostEEN.result;
 
+#read.table("/Users/simona/SGDev/BGI/project/IBD.v5/12.bacteria/abundance.list.txt",header = F,sep = "\t")->ref
+#A.CTvsC.CD.result[which(A.CTvsC.CD.result[,3]<0.2),]->test
+#cbind(as.character(ref[,2]),test[match(as.character(ref[,1]),as.character(test[,1])),1:3])
 
-lda.result[which(lda.result[,2]>3),]->temp
-write.table(temp,"temp>3",col.names = T,row.names = F,sep="\t",quote = F);
+MGS<-as.character(unique(c(CT.result[which(CT.result[,2]<0.05),1],
+                           A.CTvsB.CD.result[which(A.CTvsB.CD.result[,2]<0.05),1],
+                           A.CTvsC.CD.result[which(A.CTvsC.CD.result[,2]<0.05),1],
+                           B.result[which(B.result[,2]<0.05),1],
+                           B.CTvsC.CD.result[which(B.CTvsC.CD.result[,2]<0.05),1],
+                           B.CDvsC.CD.result[which(B.CDvsC.CD.result[,2]<0.05),1],
+                           PreEENvsPostEEN.result[which(PreEENvsPostEEN.result[,2]<0.05),1])))
+lda.data <- data.frame(Data[,match(MGS,colnames(Data))],
+                        group = label) 
+library("MASS")
+
+means<-c()
+for(i in 1:30){
+  sample(1:nrow(lda.data),117,replace = T)->sl
+  temp<-lda.data[sl,]
+  z <- lda(group ~ ., temp, prior = c(1,1,1)/3)
+  z$scaling[,1]->w
+  w.unit<-w/sqrt(sum(w^2))
+  ss<-temp[,-match("group",colnames(temp))]
+  xy.matrix<-as.matrix(ss)
+  LD<-xy.matrix%*%w.unit
+  effect.size <- mean(abs(mean(LD[temp[,"group"]=="A"]) - mean(LD[temp[,"group"]=="B"])),
+                      abs(mean(LD[temp[,"group"]=="A"]) - mean(LD[temp[,"group"]=="C"])),
+                      abs(mean(LD[temp[,"group"]=="B"]) - mean(LD[temp[,"group"]=="C"])))
+  wfinal <-  effect.size * w.unit
+  mm <- (abs(z$means["A",]-z$means["B",])+abs(z$means["B",]-z$means["C",])+abs(z$means["A",]-z$means["C",]))/3
+  coeff<-abs(wfinal)
+  value<-log((((coeff+mm)*0.5)*(1e+6)+1),10)
+  means<-cbind(means,value[match(colnames(test),names(value))])
+}
+means[which(rowMeans(means)>2),]->re
+write.table(rowMeans(re),"lefse.list.p05.lda2.v2.txt",col.names = F,row.names = T,sep = "\t",quote = F)
+
+#======table
+read.table("lefse.list.p05.lda2.v2.txt",header = F,sep = "\t")->MGS.list
+read.table("M700.Kruskal.p05.mgs.list.v2.txt",header = T,sep = "\t")->MGS.res
+colnames(MGS.list)<-c("mgsID","lda.score")
+merge(MGS.list,MGS.res,by="mgsID")->MGS.table
+write.table(MGS.table,"85MGS.table.test.txt",col.names = T,row.names = F,sep = "\t",quote = F)
 
